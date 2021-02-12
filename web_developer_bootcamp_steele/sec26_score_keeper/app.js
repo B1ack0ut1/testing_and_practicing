f4ceb5708a6aa4e1e147973857d79923b6a7452b
Jeffrey Wu <jeffreyyukaiwu@yahoo.com>
2021-02-13 01:16:00 +0800

    Completed reset of Steele's scorekeeper codealong (pt. 3 and pt. 4).

--- web_developer_bootcamp_steele/sec26_score_keeper/app.js
+++ web_developer_bootcamp_steele/sec26_score_keeper/app.js
@@ -1,37 +1,27 @@
-const p1Button = document.querySelector("#p1Button");
-const p2Button = document.querySelector("#p2Button");
-const p1Display = document.querySelector("#p1Display");
-const p2Display = document.querySelector("#p2Display");
+const p1 = {
+  score: 0,
+  button: document.querySelector("#p1Button"),
+  display: document.querySelector("#p1Display"),
+};
+
+const p2 = {
+  score: 0,
+  button: document.querySelector("#p2Button"),
+  display: document.querySelector("#p2Display"),
+};
+
 const resetButton = document.querySelector("#reset");
 const winningScoreSelect = document.querySelector("#play-to");
 
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-p1Button.addEventListener("click", () => {
-  if (!isGameOver) {
-    p1Score++;
-    if (p1Score === winningScore) {
-      isGameOver = true;
-      p1Display.classList.add("winner");
-      p2Display.classList.add("loser");
-    }
-    p1Display.textContent = p1Score; // Can innerText be assigned a number variable? Is p1Score converted to a string?
-  }
+p1.button.addEventListener("click", () => {
+  updateScores(p1, p2);
 });
 
-p2Button.addEventListener("click", () => {
-  if (!isGameOver) {
-    p2Score++;
-    if (p2Score === winningScore) {
-      isGameOver = true;
-      p2Display.classList.add("winner");
-      p1Display.classList.add("loser");
-    }
-    p2Display.textContent = p2Score; // Can innerText be assigned a number variable? Is p1Score converted to a string?
-  }
+p2.button.addEventListener("click", () => {
+  updateScores(p2, p1);
 });
 
 resetButton.addEventListener("click", () => {
@@ -43,12 +33,26 @@ winningScoreSelect.addEventListener("change", function () {
   resetGame();
 });
 
+function updateScores(player, opponent) {
+  if (!isGameOver) {
+    player.score++;
+    if (player.score === winningScore) {
+      isGameOver = true;
+      player.display.classList.add("has-text-success");
+      opponent.display.classList.add("has-text-danger");
+      player.button.disabled = true;
+      opponent.button.disabled = true;
+    }
+  }
+  player.display.textContent = player.score;
+}
+
 function resetGame() {
   isGameOver = false;
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.textContent = 0;
-  p2Display.textContent = 0;
-  p1Display.classList.remove("winner", "loser"); // Removes classes given as argument if they exist, doesn't throw error if they don't
-  p2Display.classList.remove("winner", "loser");
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.display.textContent = 0;
+    p.display.classList.remove("has-text-success", "has-text-danger"); // Removes classes given as argument if they exist, doesn't throw error if they don't
+    p.button.disabled = false;
+  }
 }
